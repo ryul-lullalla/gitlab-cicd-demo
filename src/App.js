12297c74141c56e5a75311031fb1a67383db7fb7
충률 이 <clee@energyx.co.kr>
2020-12-10 16:32:22 +0900

    feat: versioning test TRY 4
    BREAKING CHANGE: major version up Test Try 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       Stage Branch + Dev Branch + Recreate Deleted Stage Branch + CD TEST from
       Stage TEST2 ++ CZ & SR: TRY2+1
       <input value="v.1.2.1?" />i dont know how the versioning works
-      <input value="versioning stage-master try 3" />
+      <input value="versioning stage-master try 4" />
     </div>
   );
 }
