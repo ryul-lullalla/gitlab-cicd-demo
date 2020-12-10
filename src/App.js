729b3202840761b5cb2b369a5eb447bbcea7b7d5
충률 이 <clee@energyx.co.kr>
2020-12-10 10:42:49 +0900

    gitlad CD test2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       Stage Branch + Dev Branch + Recreate Deleted Stage Branch + CD TEST from
-      Stage
+      Stage TEST2
     </div>
   );
 }
