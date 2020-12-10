62bfe5118838ca2ecf9e3ab7d74d3ceef28fa405
충률 이 <clee@energyx.co.kr>
2020-12-10 10:33:02 +0900

    test1 for CD in Stage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Stage Branch + Dev Branch + Recreate Deleted Stage Branch
+      Stage Branch + Dev Branch + Recreate Deleted Stage Branch + CD TEST from
+      Stage
     </div>
   );
 }
