3ac82f73215b971646292c4ae58d54fce98a0b79
충률 이 <clee@energyx.co.kr>
2020-12-10 16:05:28 +0900

    feat: versioning test TRY 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       Stage Branch + Dev Branch + Recreate Deleted Stage Branch + CD TEST from
       Stage TEST2 ++ CZ & SR: TRY2+1
-      <input value="v.1.2.0?" />
+      <input value="v.1.2.1?" />i dont know how the versioning works
     </div>
   );
 }
